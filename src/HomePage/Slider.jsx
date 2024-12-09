e51cfb05ab6643f366471cce90ec5a58b76e3b9a
wijdane29 <wijdaneelamri4@example.com>
2024-12-09 14:16:42 +0100

    homepage modification 1

--- src/HomePage/Slider.jsx
+++ src/HomePage/Slider.jsx
@@ -20,26 +20,51 @@ export default function Slider() {
             <div className="slider">
                 <div className="slides" style={{ marginLeft: `-${currentSlide * 800}px` }}>
                     <div className="slide">
-                        <img src="images/1.jpeg" alt="Slide 1" />
+                        <img src="images/background4.jpeg" alt="Slide 1" />
                     </div>
                     <div className="slide">
-                        <img src="images/2.jpeg" alt="Slide 2" />
+                        <img src="images/13.jpg" alt="Slide 13" />
                     </div>
                     <div className="slide">
-                        <img src="images/3.jpeg" alt="Slide 3" />
+                        <img src="images/Wild Love by Elsie Silver.jpg" alt="Slide 14" />
                     </div>
                     <div className="slide">
-                        <img src="images/4.jpeg" alt="Slide 4" />
+                        <img src="images/bookheart.jpeg" alt="Slide 2" />
                     </div>
                     <div className="slide">
-                        <img src="images/5.jpeg" alt="Slide 5" />
+                        <img src="images/feefd341-5780-476e-9d47-5b2c7bda05c2.jpg" alt="Slide 2" />
                     </div>
                     <div className="slide">
-                        <img src="images/6.jpeg" alt="Slide 6" />
+                        <img src="images/fav-book1.jpeg" alt="Slide 3" />
                     </div>
                     <div className="slide">
-                        <img src="images/7.jpeg" alt="Slide 7" />
+                        <img src="images/2aca460a-eaf5-4d8e-b912-4d58f8186739.jpg" alt="Slide 4" />
                     </div>
+                    <div className="slide">
+                        <img src="images/6f9b0401-ddc6-43e7-b3cc-2c33c997db58.jpg" alt="Slide 5" />
+                    </div>
+                    <div className="slide">
+                        <img src="images/217b2955-ab0a-48df-ab92-e2dabab43c9a.jpg" alt="Slide 6" />
+                    </div>
+                    <div className="slide">
+                        <img src="images/2066d3aa-6be8-4065-8867-9e744b477555.jpg" alt="Slide 7" />
+                    </div>
+                    <div className="slide">
+                        <img src="images/bac09b86-21c4-4a64-a46b-40a57077b4af.jpg" alt="Slide 8" />
+                    </div>
+                    <div className="slide">
+                        <img src="images/Better Book series.jpg" alt="Slide 9" />
+                    </div>
+                    <div className="slide">
+                        <img src="images/c55de4b4-9a88-43d6-8f93-39a464d4fe33.jpg" alt="Slide 10" />
+                    </div>
+                    <div className="slide">
+                        <img src="images/ccbb5344-ec7b-46d2-96cd-89689b1ed612.jpg" alt="Slide 11" />
+                    </div>
+                    <div className="slide">
+                        <img src="images/fav-book1.jpeg" alt="Slide 12" />
+                    </div>
+                    
                 </div>
 
                 <div className="slider-content">
@@ -52,7 +77,7 @@ export default function Slider() {
 
 
                 <div className="navigation-manual">
-                    {Array.from({ length: 7 }).map((_, index) => (
+                    {Array.from({ length: 14 }).map((_, index) => (
                         <label
                             key={index}
                             htmlFor={`radio${index + 1}`}
